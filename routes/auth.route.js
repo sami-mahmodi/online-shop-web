02327bcfd9618c994bf9abaae355a28fcc451e2a
Samiullah Mahmodi <113135377+sami-mahmodi@users.noreply.github.com>
2023-06-07 21:32:27 +0500

    signup form created

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 const authController = require("../controllers/auth.controller");
 router.get("/signup", authController.getSignup);
+router.post("/signup", authController.signup);
 router.get("/signup", authController.getLogin);
 module.exports = router;
